f3f096638fbeb4102d48a5450ea76944184dba6e
qixinbo <qixinbo@gmail.com>
2022-02-15 14:51:40 +0800

    first imjoy app

--- hello-world-cli/src/components/HelloWorld.vue
+++ hello-world-cli/src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+  <div>
+<!--     <h1>{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -26,25 +26,57 @@
       <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    </ul> -->
   </div>
 </template>
 
 <script>
 import * as imjoyCore from 'imjoy-core'
-
-console.log("--------------")
-console.log(imjoyCore)
-console.log("--------------")
-
 const imjoy = new imjoyCore.ImJoy({
     imjoy_api: {},
     //imjoy config
 });
 
+imjoy.event_bus.on("add_window", w => {
+      const container = document.createElement('div');
+      container.id = w.window_id; // <--- this is important
+      container.style.backgroundColor = '#ececec';
+      container.style.height = "800px";
+      container.style.width = "100%";
+      // Here we simply add to the body
+      // but in reality, you can embed it into your UI
+      document.body.appendChild(container)
+});
+
 imjoy.start({workspace: 'default'}).then(async ()=>{
     console.log('ImJoy started');
-})
+    await imjoy.api.alert("hello world");
+    // await imjoy.api.prompt('what is your name?')
+    await imjoy.api.createWindow(
+    {
+      name: "My window",
+      // type: "imjoy/generic",
+      src: "https://raw.githubusercontent.com/imjoy-team/imjoy-plugins/master/repository/welcome.imjoy.html"
+    })
+    // const grid = await imjoy.api.createWindow({
+    //     src: "https://grid.imjoy.io/#/app",
+    //     name: "Grid",
+    // });
+    // await grid.createWindow({ src: "https://kaibu.org" });
+});
+// imjoy.api.createWindow({
+//     src: "https://grid.imjoy.io/#/app",
+//     name: "Grid",
+// });
+console.log(imjoy)
+console.log("------------------")
+console.log(window)
+// import { imjoyRPC } from 'imjoy-rpc';
+
+// imjoyRPC.setupRPC({name: 'My Awesome App'}).then(() => {
+
+// })
+console.log("------------------")
 
 
 export default {
