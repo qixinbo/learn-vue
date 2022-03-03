e9f0cc6538f9c179925721e32e5bb9b0f0c4eb77
qixinbo <qixinbo@gmail.com>
2022-03-03 17:23:12 +0800

    vue updated

--- vue-manage/src/components/Imjoy.vue
+++ vue-manage/src/components/Imjoy.vue
@@ -1,8 +1,19 @@
 <template>
   <div class="imjoy noselect">
+    <!-- 整个imjoy页面框架是通过vue material这个组件库的md-app进行组织的 -->
+    <!-- 它包括了md-app-toolbar工具栏、 md-app-drawer菜单栏和md-app-content内容区三部分 -->
+    <!-- 相关教程见：https://www.creative-tim.com/vuematerial/components/app -->
     <md-app>
+      <!-- 工具栏就是最上面的横条，里面的内容直接全部删除了，仅保留架子 -->
       <md-app-toolbar class="md-dense app-toolbar" md-elevation="0">
       </md-app-toolbar>
+
+      <!-- 接下来就是左侧的菜单栏 -->
+      <!-- drawer的属性设置见：https://www.creative-tim.com/vuematerial/components/drawer# -->
+      <!-- 具体地： -->
+      <!-- (1)菜单栏是否可见是通过md-active属性控制，其通过v-bind绑定到了menuVisible这个变量上 -->
+      <!-- (2)菜单栏关闭和打开两个事件都通过v-on绑定到了wm.resizeAll()函数上 -->
+      <!-- (3)菜单栏是否常驻md-persistent和菜单栏是否支持触屏下的swipe绑定到了screenWidth这个变量上 -->
       <md-app-drawer
         :md-active.sync="menuVisible"
         @md-closed="wm.resizeAll()"
@@ -10,8 +21,24 @@
         :md-persistent="screenWidth > 800 ? 'full' : null"
         :md-swipeable="screenWidth > 600 ? false : true"
       >
+        <!-- 接下来是将上传文件,添加工作流,添加插件和插件列表都放在了一个card中 -->
+        <!-- 这个card的显示有两个判断语句,一个是用v-show判断plugin_loaded这个变量是否为true -->
+        <!-- 另一个是用v-if判断pm这个变量是否为true, -->
+        <!-- v-if和v-show看起来差不多,但有区别,见:https://cn.vuejs.org/v2/guide/conditional.html -->
         <md-card id="plugin-menu" v-show="plugin_loaded" v-if="pm">
+          <!-- 将files,workflow和plugins都放在了card的header中, -->
+          <!-- 这里我们将files和workflow都删掉了,仅留plugins的代码 -->
           <md-card-header>
+            <!-- 添加plugins就是一个按钮,其class也是由v-bind绑定并判断 -->
+            <!-- 该按钮的点击时间用v-on绑定到了showPluginManagement()函数上 -->
+            <!-- 当点击该按钮后, 从而执行该函数,其中有一个非常重要的变量:-->
+            <!-- this.showAddPluginDialog会设为true -->
+            <!-- 从而会引发如下对话框的弹出:
+            <md-dialog
+              class="plugin-dialog"
+              :md-active.sync="showAddPluginDialog"
+              :md-click-outside-to-close="true"
+            > -->
             <md-button
               ref="add_plugin_button"
               :class="pm.installed_plugins.length > 0 ? '' : 'md-primary'"
@@ -21,59 +48,79 @@
             </md-button>
           </md-card-header>
 
+          <!-- 接下来就是将插件列表放在card的content区域 -->
           <md-card-content>
+            <!-- 使用v-for来循环插件列表,并对每一项提供了key的身份 -->
+            <!-- v-for的教程见:https://cn.vuejs.org/v2/guide/list.html -->
             <div v-for="plugin in sortedRunnablePlugins()" :key="plugin.name">
+              <!-- 增加一个分割线 -->
               <md-divider></md-divider>
               <div style="display: flex;">
+                <!-- 这里对每一个插件会包装一个badge徽章,用途是判断它需不需要升级 -->
+                <!-- 如果需要升级,会出现一个NEW的角标 -->
                 <md-badge
                   :class="plugin.update_available ? '' : 'hide-badge'"
                   class="md-square md-primary"
                   md-dense
                   md-content="NEW"
                 >
+                  <!-- 这里是对插件的icon按钮点击后会出现的菜单进行定义 -->
                   <md-menu md-size="medium">
+                    <!-- 触发菜单的icon按钮定义 -->
                     <md-button
                       class="md-icon-button"
                       :class="plugin.running ? 'md-accent' : ''"
                       md-menu-trigger
                     >
+                      <!-- 插件的加载状态 -->
                       <md-progress-spinner
                         v-if="plugin.initializing || plugin.terminating"
                         class="md-accent"
                         :md-diameter="20"
                         md-mode="indeterminate"
                       ></md-progress-spinner>
+                      <!-- icon会判断该插件有没有提供自定义的图标,如果没有,则使用默认的extension图标 -->
+                      <!-- 这个地方调用的就是之前入口文件所全局注册的PluginIcon组件 -->
                       <plugin-icon
                         v-else
                         :icon="plugin.config.icon"
                       ></plugin-icon>
+                      <!-- 按钮的工具提示信息 -->
                       <md-tooltip v-if="screenWidth > 500">{{
                         plugin.name + ": " + plugin.config.description
                       }}</md-tooltip>
                     </md-button>
 
+                    <!-- 点击icon按钮后弹出的菜单选项 -->
                     <md-menu-content>
+                      <!-- Docs选项,绑定了showDoc方法 -->
                       <md-menu-item @click="showDoc(plugin.id)">
                         <md-icon>description</md-icon>Docs
                       </md-menu-item>
+                      <!-- Share选项,不一定会显示,有一个v-if条件渲染 -->
                       <md-menu-item
                         v-if="plugin.config.origin"
                         @click="sharePlugin(plugin.id)"
                       >
                         <md-icon>share</md-icon>Share
                       </md-menu-item>
+                      <!-- Export选项,绑定了downloadPlugin -->
                       <md-menu-item @click="downloadPlugin(plugin.id)">
                         <md-icon>cloud_download</md-icon>Export
                       </md-menu-item>
+                      <!-- Edit选项,绑定了editPlugin -->
                       <md-menu-item @click="editPlugin(plugin.id)">
                         <md-icon>edit</md-icon>Edit
                       </md-menu-item>
+                      <!-- Reload选项,绑定了reloadPlugin -->
                       <md-menu-item @click="reloadPlugin(plugin.config)">
                         <md-icon>autorenew</md-icon>Reload
                       </md-menu-item>
+                      <!-- Terminate选项,绑定了unloadPlugin -->
                       <md-menu-item @click="unloadPlugin(plugin)">
                         <md-icon>clear</md-icon>Terminate
                       </md-menu-item>
+                      <!-- Remove选项,绑定了removePlugin -->
                       <md-menu-item
                         class="md-accent"
                         @click="removePlugin(plugin)"
@@ -84,6 +131,11 @@
                     </md-menu-content>
                   </md-menu>
                 </md-badge>
+
+                <!-- 接下来就是由插件的名称所形成的按钮 -->
+                <!-- 对于需要链接到engine的插件,还会检测其状态,如果没有链接到engine上,就会disable该按钮,从而无法运行. -->
+                <!-- 对于鼠标操作,也附加了鼠标按钮修饰符,防止误操作,教程见:https://cn.vuejs.org/v2/guide/events.html -->
+                <!-- 点击该按钮后就会运行runOp方法 -->
                 <md-button
                   class="joy-run-button"
                   :class="
@@ -106,30 +158,15 @@
 
               </div>
 
+              <!-- 这个地方是ImJoy的核心!! -->
+              <!-- 此处也是ImJoy的名称的来源,即ImJoy来自于joy.js这个库 -->
+              <!-- 这个地方需要后面仔细研究机理 -->
               <div
                 v-for="op in plugin.ops"
                 :key="op.plugin_id + op.name"
-                v-show="plugin.panel_expanded"
+                v-show="true"
               >
-                <md-button
-                  v-if="op.name != plugin.name"
-                  class="md-icon-button"
-                  :disabled="false"
-                >
-                  <md-icon>chevron_right</md-icon>
-                </md-button>
-                <md-button
-                  v-if="op.name != plugin.name"
-                  class="joy-run-button md-primary op-button"
-                  :class="plugin.running ? 'md-accent' : 'md-primary'"
-                  :disabled="plugin._disconnected"
-                  @click.exact="runOp(op)"
-                  @click.right.exact="logPlugin(plugin)"
-                >
-                  {{ op.name }}
-                </md-button>
-
-                <joy :config="op" :show="plugin.panel_expanded || false"></joy>
+                <joy :config="op" :show="true"></joy>
               </div>
               <md-divider></md-divider>
             </div>
@@ -138,10 +175,12 @@
         </md-card>
       </md-app-drawer>
 
+      <!-- 接下来就是整个界面的第三部分,即内容区 -->
       <md-app-content
         :class="workspace_dropping ? 'file-dropping' : ''"
         class="whiteboard-content"
       >
+        <!-- 就是引用了whiteboard这个组件 -->
         <whiteboard
           v-if="wm"
           id="whiteboard"
@@ -150,6 +189,8 @@
           :window-manager="wm"
         ></whiteboard>
       </md-app-content>
+
+    <!-- 以上就是整个界面的布局 -->
     </md-app>
 
     <md-dialog-alert
@@ -1740,18 +1781,6 @@ export default {
         this.menuVisible = false;
       }
     },
-    selectFileChanged(event) {
-      // console.log(event.target.files)
-      this.selected_file = event.target.files[0];
-      this.selected_files = event.target.files;
-      //normalize relative path
-      for (let i = 0; i < event.target.files.length; i++) {
-        const file = event.target.files[i];
-        file._path = file.webkitRelativePath;
-        file.loaders = this.wm.getDataLoaders(file);
-      }
-      this.loadFiles(this.selected_files);
-    },
 
     //#################ImJoy API functions##################
     showSnackbar(_plugin, msg, duration) {
@@ -1764,74 +1793,7 @@ export default {
       this.show_snackbar = true;
       this.$forceUpdate();
     },
-    async showFileDialog(_plugin, config) {
-      config = config || {};
-      let _return_array = true;
-      if (_plugin && _plugin.id) {
-        if (!config.file_manager) {
-          if (_plugin.api.FILE_MANAGER_URL) {
-            config.file_manager = this.fm.getFileManagerByUrl(
-              _plugin.api.FILE_MANAGER_URL
-            );
-          }
-        } else {
-          if (typeof config.file_manager === "string")
-            config.file_manager = this.fm.getFileManagerByUrl(
-              config.file_manager
-            );
-        }
-        // assert(config.file_manager, "No file manager is selected.");
-        config.root =
-          config.root || (_plugin.config && _plugin.config.work_dir);
 
-        config.uri_type = config.uri_type || "path";
-        if (config.root && typeof config.root !== "string") {
-          throw "You need to specify a root with string type ";
-        }
-        //TODO: remove this in the future
-        if (
-          _plugin.config.api_version &&
-          utils.compareVersions(_plugin.config.api_version, "<=", "0.1.3")
-        ) {
-          config.return_object =
-            config.return_object === undefined ? false : config.return_object;
-        } else {
-          config.return_object =
-            config.return_object === undefined ? true : config.return_object;
-        }
-
-        //TODO: remove this in the future
-        if (
-          _plugin.config.api_version &&
-          utils.compareVersions(_plugin.config.api_version, "<", "0.1.8")
-        ) {
-          _return_array = false;
-        }
-      }
-      let ret;
-
-      if (
-        _return_array &&
-        config.file_manager &&
-        config.file_manager.api.showFileDialog
-      ) {
-        ret = await config.file_manager.showFileDialog(config);
-      } else {
-        if (config.file_manager && config.hide_unselected) {
-          this.selected_file_managers = [config.file_manager];
-        } else {
-          this.selected_file_managers = this.fm.fileManagers;
-        }
-        ret = await this.$refs["file-dialog"].showDialog(_plugin, config);
-      }
-
-      if (_return_array) {
-        if (!ret) return [];
-        else if (!Array.isArray(ret)) return [ret];
-        else return ret;
-      }
-      return ret;
-    },
     uploadFileToUrl(_plugin, config) {
       if (typeof config !== "object" || !config.file || !config.url) {
         throw "You must pass an object contains keys named `file` and `url`";
