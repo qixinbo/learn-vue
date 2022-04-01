c6ffc1a7ad97ae7a021d94a870462f248e5e5d57
qixinbo <qixinbo@gmail.com>
2022-04-01 15:17:41 +0800

    component finish

---
+++ vue2/02_vue组件/单文件组件.vue
@@ -0,0 +1,27 @@
+<template>
+	<div>
+		<h1>School: {{ schoolName }}</h1>
+		<button @click="showName"> 点我提示学校名 </button>
+		<student/>
+	</div>	
+</template>
+
+<script>
+	export default {
+		name: "School",
+		data(){ // data必须写成函数形式
+			return{
+				schoolName: 'peking'
+			}
+		},
+		methods:{
+			showName(){
+				alert(this.schoolName)
+			}
+		},
+	}
+</script>
+
+<style>
+	
+</style>
