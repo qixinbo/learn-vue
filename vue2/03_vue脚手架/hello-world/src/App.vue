08e88867a296f3ff2b004ae28345da77346f5de6
qixinbo <qixinbo@gmail.com>
2022-04-07 10:18:02 +0800

    slot added

--- vue2/03_vue脚手架/hello-world/src/App.vue
+++ vue2/03_vue脚手架/hello-world/src/App.vue
@@ -1,42 +1,17 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App">
-      <img src="./assets/logo.png">
-    </HelloWorld>
-    <button @click="obtainInfo">获取信息</button>
-    <ul>
-      <li v-for='pet in pets' :key="pet.id">
-        {{ pet.name }}
-      </li>
-    </ul>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
     HelloWorld
-  },
-  data(){
-    return {pets:''}
-  },
-  methods:{
-    obtainInfo(){
-      // 一定要用localhost而不是127.0.0.1
-      axios.get('http://localhost:8080/api/pets').then(
-        response => {
-          console.log('成功', response.data)
-          this.pets = response.data
-        },
-        error => {
-          console.log('失败')
-        }
-        )
-    }
   }
 }
 </script>
